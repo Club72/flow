daa4c88e613eafbcd2aac8f9724dcb0253e5bcc7
Club72 <80535265+Club72@users.noreply.github.com>
2021-03-30 14:12:05 +0700

    Delete ataovo.sh

--- ataovo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-POOL=stratum+tcp://eth-pool.beepool.org:9530
-WALLET=denovb33
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-ovo
-
-cd "$(dirname "$0")"
-
-chmod +x ./ovo && sudo ./ovo -a ethash -o $POOL -u $WALLET.$WORKER $@
